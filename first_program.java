36680d8489cbd2db1010e48bb88f6915392028e1
Badhon Nandi <116469086+badhonnandi1@users.noreply.github.com>
2025-03-15 16:19:02 +0600

    .

---
+++ first_program.java
@@ -0,0 +1,18 @@
+public class first_program
+{
+    public static void main(String [] args)
+    {   
+        // amra single line e print korte chaile (jodi cursur porer line e jabe seta na chai evabe print use korbo sudhu)
+        System.out.print("First Output : Jeta porer line e jabena");
+
+        // jodi chai porer line e curser ta jak taile println use korbo
+
+        System.out.println("Eita abr porer line e jabe");
+        
+        //jodi kono number ekta formate kore print korte chai in that case amra printf use korte pari, eitayo porer line e jabena se khatre \n use korle porer line e chole jabe
+
+
+        double de = 45.321;
+        System.err.printf("Ami ebar ekta formate e print dicchi = %.2f\n",de);
+    }
+}

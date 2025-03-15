5c31f5923add40c326618578a414ae48670ac8d4
Badhon Nandi <116469086+badhonnandi1@users.noreply.github.com>
2025-03-15 17:41:07 +0600

    taking_input

---
+++ takinginput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class takinginput {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in); //creating scanner object sc(ei name ta eccha moto)
+        // System.out.println("Please enter a number:");
+        // int num1 = sc.nextInt();
+        // System.out.println(num1);
+        
+        // double double1 = sc.nextDouble();
+        // System.out.println(double1);
+
+        // System.out.println("Please enter a number:");
+
+
+        String sx = sc.nextLine();
+        System.out.println(sx);
+
+
+        String s1 = sc.next(); //first space obdi nebe
+        System.out.println(s1);
+    }
+}
